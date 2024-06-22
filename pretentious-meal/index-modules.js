6d721b7b1de2ea15bbe66beb854e09870d4c0ec7
Simon Gordon <simon.gordon@tui.co.uk>
2024-06-22 02:11:54 +0100

    adding Pretentious Meal generator

---
+++ pretentious-meal/index-modules.js
@@ -0,0 +1,4 @@
+const chalk = require('chalk');
+const cards = require('./modules/data');
+const shuffleArray = require('./modules/shuffleArray');
+
---
+++ pretentious-meal/modules/data.js
@@ -0,0 +1,17 @@
+const adjectives = [
+  'sumptious',
+  'succulent',
+  'orgasmic',
+];
+
+
+const finishes = [
+  'topped',
+  'finished',
+  'garnished',
+];
+
+const finishAmount = [
+  'a pinch of',
+  'a generous helping of',
+];
